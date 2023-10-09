591d37356bf8d6f54e91503a02f6ff1e002abfc2
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-10-09 22:21:05 +0300

    created the error & not-found middleware

--- api/index.js
+++ api/index.js
@@ -2,19 +2,27 @@ const express = require('express')
 const app = express()
 const connectDB = require('./db/connect')
 require('dotenv').config();
+const { errorHandler } = require('./middleware/error');
+const { notFound } = require('./middleware/not-found');
+
+
+// Not Found
+app.use(notFound)
+// Error Handler Middleware
+app.use(errorHandler);
 
 // Running The Server
 const PORT = process.env.PORT || 8000
 
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_CLOUD_URI);
-    app.listen(PORT, () =>
-      console.log(`Server is listening on port ${PORT}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await connectDB(process.env.MONGO_CLOUD_URI);
+        app.listen(PORT, () =>
+            console.log(`Server is listening on port ${PORT}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
