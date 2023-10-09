93dafda322f76d1e77982af981a06863fc666f12
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-10-09 22:28:18 +0300

    created the user schema and model

---
+++ api/schemas/user.schema.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose")
+
+// User Schema 
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Please provide a username'],
+        trim: true,
+        minlength: 5,
+        maxlength: 100,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide email"],
+        trim: true,
+        minlength: 5,
+        maxlength: 100,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please provide a valid email",
+        ],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        trim: true,
+        minlength: 8,
+    },
+    profilePhoto: {
+        type: Object,
+        default: {
+            url: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png",
+            publicId: null
+        }
+    },
+    bio: {
+        type: String
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    isAccountVerified: {
+        type: Boolean,
+        default: false
+    },
+}, {
+    timestamps: true,
+}
+)
+
+
+module.exports = { UserSchema }
