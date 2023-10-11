746c1fb7b139e61541b0a2a153c1d125003da071
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-10-11 13:52:15 +0300

    created the comment schema and model

---
+++ api/models/comment.model.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const { CommentSchema } = require("../schemas/comment.schema");
+
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = { Comment, validateCreateComment, validateUpdateComment }
