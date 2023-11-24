3d2f60846084e8a1b53c00b37b5db915e9b30396
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-11-24 23:22:36 +0200

    fixed typescript bug

--- client/redux/provider.tsx
+++ client/redux/provider.tsx
@@ -1,8 +1,7 @@
-"use client";
-
+import { ReactNode } from "react";
 import { store } from "./store";
 import { Provider } from "react-redux";
 
-export function ReduxProvider({ children }: { children: React.ReactNode }) {
+export function ReduxProvider({ children }: { children: ReactNode }) {
   return <Provider store={store}>{children}</Provider>;
 }
